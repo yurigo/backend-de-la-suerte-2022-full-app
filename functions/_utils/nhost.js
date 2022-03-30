53a90b2fe7a3d5fb28382e5ba7d898ac55238a97
Yuri <ygonzoo@gmail.com>
2022-03-30 23:38:40 +0200

    NHOST_BACKEND_URL

--- functions/_utils/nhost.js
+++ functions/_utils/nhost.js
@@ -1,10 +1,7 @@
-require("dotenv").config();
-console.log(process.env.VITE_NHOSTS_URL);
-
 import { NhostClient } from '@nhost/nhost-js'
 
 const nhost = new NhostClient({
-   backendUrl: process.env.VITE_NHOSTS_URL
+   backendUrl: process.env.NHOST_BACKEND_URL
 })
 
 export { nhost }
