7367f273eaf5a7a970bfb5c40620ba821f7efe83
Yuri <ygonzoo@gmail.com>
2022-04-03 22:05:17 +0200

    removed line 27 on hit to avoid premature response

--- functions/hit.js
+++ functions/hit.js
@@ -24,7 +24,7 @@ export default async (req, res) => {
         headers: req.headers
     });
 
-    res.json({token: token});
+    // res.json({token: token});
 
     nhost.graphql.setAccessToken(token);
 
