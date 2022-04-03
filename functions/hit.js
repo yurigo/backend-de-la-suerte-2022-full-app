a39b2e938b789b890d28fee5db2735d42a2ffb9d
Yuri <ygonzoo@gmail.com>
2022-04-03 22:02:45 +0200

    new hit function!

--- functions/hit.js
+++ functions/hit.js
@@ -1,5 +1,9 @@
 import { nhost } from "./_utils/nhost";
 
+
+const cinco = 5;
+const contador = 0;
+
 export default async (req, res) => {
     
     res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -22,6 +26,8 @@ export default async (req, res) => {
 
     res.json({token: token});
 
+    nhost.setAccessToken(token);
+
     // const mutationResponse = await nhost.graphql.request(`
     //     mutation FoundGold($_eq: uuid!) {
     //         insert_mina_mina(objects: {}, on_conflict: {constraint: mina_pkey}) {
@@ -37,6 +43,26 @@ export default async (req, res) => {
     // )
 
     // res.json({ mutationResponse})
+
+    let encontradoOro = false;
+    contador ++;
+
+    if (contador === cinco) {
+        contador = 0;
+        encontradoOro = true
+    }
+
+    const hitResponse = await nhost.graphql.request(`
+        mutation MyMutation($oro: Boolean = false) {
+          insert_mina_pico(objects: {oro: $oro}) {
+            affected_rows
+          }
+        }
+      `,
+          { oro: encontradorOro }
+      );
+
+    res.json({ grapql: hitResponse, contador: contador , encontradoOro: encontradoOro , cinco: cinco });
 
 
 }
